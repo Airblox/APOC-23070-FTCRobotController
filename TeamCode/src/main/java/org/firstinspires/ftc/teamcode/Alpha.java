3e6b9f829c6cc5b73cf8a0c91f48e257468dc8fb
Airblox <106415728+Airblox@users.noreply.github.com>
2024-06-30 23:20:59 +0800

    MeepMeep Visualiser - Autonomous OpMode - MeepMeep Visualiser configured

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alpha.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous(name="Autonomous v0 Alpha Build (Red/Close)")
+public class Alpha extends LinearOpMode {
+    private final static String BUILD_VERSION = "0.0.0";
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+    }
+}
