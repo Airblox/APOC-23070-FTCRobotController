c357f12fea299f5660473180db69c18bde48aa1b
Airblox <106415728+Airblox@users.noreply.github.com>
2024-07-07 19:50:47 +0800

    Roadrunner 0.5.6 Transition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alpha.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alpha.java
@@ -1,27 +1,26 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
-
-@Config
-@Autonomous(name="Autonomous v0 Alpha Build (Red/Close)")
-public class Alpha extends LinearOpMode {
-    private final static String BUILD_VERSION = "0.0.0";
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        Project1Hardware robot;
-        MecanumDrive drive;
-
-        waitForStart();
-        while (opModeIsActive()) {}
-    }
-
-    enum States {
-        PATH_TO_PURPLE,
-        SCORING_PURPLE,
-    }
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.acmerobotics.dashboard.config.Config;
+//import com.acmerobotics.roadrunner.drive.MecanumDrive;
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//
+//@Config
+//@Autonomous(name="Autonomous v0 Alpha Build (Red/Close)")
+//public class Alpha extends LinearOpMode {
+//    private final static String BUILD_VERSION = "0.0.0";
+//
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//        Project1Hardware robot;
+//        MecanumDrive drive;
+//
+//        waitForStart();
+//        while (opModeIsActive()) {}
+//    }
+//
+//    enum States {
+//        PATH_TO_PURPLE,
+//        SCORING_PURPLE,
+//    }
+//}
